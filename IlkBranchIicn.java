3f4edd088fff42afbe92f6a69ce8295bcfee3c12
Şahin <67071153+Gunesahin@users.noreply.github.com>
2023-08-11 21:43:35 +0200

    Update IlkBranchIicn.java

--- IlkBranchIicn.java
+++ IlkBranchIicn.java
@@ -1,6 +1,8 @@
 public class IlkBranchIicn {
     public static void main(String[] args) {
         System.out.println("Hey bu benim ilk comitim");
+        System.out.println("Hey bu benim Main de ilk comitim");
+            
     }
     // bu benim ilk comitim olacak
 
