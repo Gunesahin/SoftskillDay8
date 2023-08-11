2d36995e86b4908a6b5ff0b99eb46fa4adc104f3
azra <sahinespana@gmail.com>
2023-08-11 22:33:38 +0200

    son

--- IlkBranchIicn.java
+++ IlkBranchIicn.java
@@ -2,6 +2,10 @@ public class IlkBranchIicn {
     public static void main(String[] args) {
         System.out.println("Hey bu benim ilk comitim");
         System.out.println("Hey bu benim Main de ilk comitim");
+        for (String arg : args) {
+            System.out.println("Ne yaptin sen");
+        }
+
             
     }
     // bu benim ilk comitim olacak
